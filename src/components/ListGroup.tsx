e7f2acbeab7bfee841caa9a45e5ef9c43ae66e4d
mboyes15 <115404104+mboyes15@users.noreply.github.com>
2025-12-10 18:26:35 +1100

    set up, play about and test environment

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,45 @@
+// import type { MouseEvent } from "react";
+
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  //  Hook: telling react that the state will change
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    // using a fragment when <>
+    <>
+      <h1>{heading}</h1>
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              {
+                setSelectedIndex(index);
+                onSelectItem(item);
+              }
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
